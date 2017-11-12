cb47b4dc21bfc9dce2fe7ddebbbe052186a7ceb8
yourfather <yourfather@163.com>
2017-11-12 23:39:16 +0800

    前台首页及分类页侧边栏样式完成

---
+++ public/front/js/commen.js
@@ -0,0 +1,13 @@
+
+// 区块滚动功能
+mui('.mui-scroll-wrapper').scroll({
+    indicators: false, //是否显示滚动条
+	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+});
+
+// 轮播图自动轮播功能
+//获得slider插件对象
+var gallery = mui('.mui-slider');
+gallery.slider({
+  interval:1000//自动轮播周期，若为0则不自动播放，默认为0；
+});
